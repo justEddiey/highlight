2e48db1f833c06e330c0c4f89acf3c612a1ced96
Edet Edidiong <83464906+justEddiey@users.noreply.github.com>
2022-04-24 21:20:09 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-const lists=['Act of violence','Categorus','sportusdictys','Biblicalnames','Dedstitusde','Boat']
+
 var texts=document.getElementById('texts');
 
 var slt=''
 
-document.body.innerHTML+=`<div style="color:white;display:none;padding:12px;cursor:pointer;" id="tooltip" onclick="addhighlight()">add highlight</div>`
+document.body.innerHTML+=`<div style="color:white;display:none;padding:12px;cursor:pointer,borderRadius:10px"; id="tooltip" onclick="addhighlight()">add highlight</div>`
 
 var tooltip=document.getElementById('tooltip');
 function getRangeObject(object){
@@ -28,26 +28,15 @@ document.onmouseup=function(e){
             tooltip.style.display='block';
             tooltip.innerHTML='addhighlight';
             if(rect.y>30){
-                tooltip.style.top=rect.top-40+'px';
+                tooltip.style.top=e.clientY+window.scrollY-40+'px';
             }
             else{
                 tooltip.style.top=rect.top+40+'px';
             }
             
-            tooltip.style.left=rect.x+5+'px';
+            tooltip.style.left=e.clientX+5+'px';
             tooltip.style.position='absolute';
             tooltip.style.background='black';
-            console.log(rect)
-
-        //     var contents=rangeobject.extractContents()
-        // var node=document.createElement('div');
-        // node.className='highlight'
-        // node.appendChild(contents)  
-        // rangeobject.insertNode(node)
-        // var childnode=document.createElement('div');
-        // childnode.className='tag';
-        // childnode.innerHTML='"'
-        // node.append(childnode)
 
         }
 
@@ -56,23 +45,39 @@ document.onmouseup=function(e){
     }
 
     }
+   
 
 }
 
 function addhighlight(){
     event.preventDefault()
-    console.log(slt)
-       var contents=slt.extractContents()
-        var node=document.createElement('div');
+    
+    //    var contents=slt.extractContents()
+        var node=document.createElement('span');
         node.className='highlight'
-        node.appendChild(contents)  
-        slt.insertNode(node)
-        var childnode=document.createElement('div');
-        childnode.className='tag';
-        childnode.innerHTML='"'
-        node.append(childnode)
+        node.addEventListener("click",means)
+        // node.appendChild(contents) 
+        // slt.insertNode(node)
+        // var childnode=document.createElement('div');
+        // childnode.className='tag';
+        // childnode.innerHTML='"'
+        // node.append(childnode)
+        slt.surroundContents(node);
         tooltip.style.display='none';
 
+}
+function means(){
+    // console.log(event.target)
+    var innertext=event.target.innerHTML
+    var range = document.createRange()
+    var anchorTag = event.target;
+    range.selectNodeContents(event.target)
+    anchorTag.insertAdjacentHTML('afterend', innertext);
+    // range.extractContents()
+    event.target.remove()
+    // var afterHtml = anchorTag.innerHTML.substr(range.startOffset);
+  
+      
 }
 function shows(){
     var range=selection.getRangeAt(0);
@@ -84,13 +89,9 @@ function shows(){
 
 function create(range){
     var node=document.createElement('div');
-    // var childnode=document.createElement('div');
-    // childnode.className='tag'
+
     node.className='highlight'
-    // texts.append(node)
-    // node.innerHTML='me'
-    // node.append(childnode)
-    // childnode.innerHTML='%'
+    
     node.appendChild(range)
     range.surroundContents(node)
     
